c60f33cb240e54312425911e9b9a0ad545ab631d
Baalavignesh <baalavignesh21@gmail.com>
2024-02-15 20:45:18 +0530

    added convex

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,19 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store/store.ts";
-import { PersistGate } from "redux-persist/integration/react"; 
+import { PersistGate } from "redux-persist/integration/react";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <ConvexProvider client={convex}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </ConvexProvider>
     </Provider>
   </React.StrictMode>
 );
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -2,19 +2,24 @@ import { useEffect } from "react";
 import { Container, Fade } from "@mui/material";
 import NavBar from "../../shared/navbar";
 import CustomButton from "../../shared/button";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 const Dashboard : React.FC =() => {
   useEffect(() => {
     console.log('dashboard screen');
   }, [])
 
+  const tasks = useQuery(api.tasks.get);
   return (
     <div>
       <NavBar />
       <Fade in={true} timeout={1500}>
       <Container maxWidth="xl">
       <h1 className="mt-8 mb-8">Dashboard Screen</h1>
-
+      {tasks?.map((val:any, index:number) => (
+        <div key={index}>{val.text}</div>
+      ))}
       <CustomButton label="Dummy Button" type="normal" color="primary" />
 
       </Container>
